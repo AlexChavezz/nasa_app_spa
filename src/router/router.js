5580c9dc5d6f625d9ff8abd09e2805464a287e22
Alexis Chavez <alexischavez1234.ac@gmail.com>
2021-07-21 14:11:14 -0500

    commit

--- src/router/router.js
+++ src/router/router.js
@@ -3,16 +3,22 @@ import   next  from '../controllers/next';
 
 
 export const router = async ( router ) => {
+    
     const root  = document.querySelector('#root');
     root.innerHTML = '';
-    switch ( router ){  
+    switch ( router ){
+        case '': {
+            root.innerHTML =`<h1>You're in home</h1>`;
+        }
+        break;
         case '#/PictureOfDay': {
             root.appendChild( await pictureOfDay() )  
         }
         break;
-        case '#/NeoWs': {
+        /*case '#/NeoWs': {
             root.appendChild( await next() );
         }
-        break;
+        break;*/
     }
+
 } 
