e7c4c8e4d51292fe094f472c51a2f171dbbd705b
Alexis Chavez <alexischavez1234.ac@gmail.com>
2021-07-21 19:12:59 -0500

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js',
     },
     module: {
